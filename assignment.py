f4300adbfa32da5aeea67fe3396dec53391e9355
Arjun97das <158080421+Arjun97das@users.noreply.github.com>
2024-02-02 17:42:02 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,10 @@
+name = input("Enter your name:- ")
+age = input("Enter your age:- ")
+gender = input("Enter your gender (M/F):- ")
+weight = input("Enter your weight(kgs):- ")
+
+
+print("Your name:-",name)
+print("Your age:-",age)
+print("Your gender:-",gender)
+print("Your weight:-",weight)
